b31238e3e20f2f875fe015d025cf153d79df528f
wjdwodhks35 <110615719+wjdwodhks35@users.noreply.github.com>
2023-02-04 23:20:20 +0900

    homework.1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("안녕하세요.");
+            Console.WriteLine("저의 이름은 정재완 입니다.");
+            Console.WriteLine("저는 06년생이며, 만 16세입니다.");
+            Console.WriteLine("저의 직업는 학생이며, 인제고등학교에 재학중입니다.");
+            Console.WriteLine("저는 잠자기와 컴퓨터하는 것을 좋아합니다.");
+            Console.WriteLine("그리고 사람과 대화하는 것은 좋아하지만 사람을 만나는건 싫어합니다.");
+            Console.WriteLine("저의 꿈은 게임 개발자입니다.");
+            Console.WriteLine("올해 목표는 간단한 게임 4개 정도 만들기 입니다.");
+            Console.WriteLine("그리고 이제 쓸게 없네요.");
+            Console.WriteLine("마지막 줄~");
+        }
+    }
+}
